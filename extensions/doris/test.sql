704fef8bdef1c081814d95195582e81fa12b1d3f
朱文康 <1903215403@qq.com>
2026-05-28 11:44:42 +0800

    doris自动识别表名

--- extensions/doris/test.sql
+++ extensions/doris/test.sql
@@ -1,21 +1,43 @@
 CREATE DATABASE IF NOT EXISTS test_db;
 
-DROP TABLE IF EXISTS test_db.wp_nginx;
-CREATE TABLE test_db.wp_nginx (
-    wp_event_id BIGINT COMMENT '事件唯一ID',
-    wp_src_key STRING COMMENT '数据来源表示',
-    sip STRING COMMENT '客户端IP',
-    `timestamp` STRING COMMENT '原始时间字符串',
-    `http/request` STRING COMMENT 'HTTP请求行',
-    status SMALLINT COMMENT 'HTTP状态码',
-    size INT COMMENT '响应大小(byte)',
-    referer STRING COMMENT '来源页面',
-    `http/agent` STRING COMMENT 'User-Agent'
+DROP TABLE IF EXISTS test_db.wp_nginx_01;
+CREATE TABLE test_db.wp_nginx_01 (
+                                  `timestamp` bigint COMMENT '原始时间字符串',
+                                  wp_event_id STRING COMMENT '事件唯一ID',
+                                  wp_src_key STRING DEFAULT "" COMMENT '数据来源表示',
+                                  sip STRING DEFAULT "" COMMENT '客户端IP',
+                                  `http/request` STRING DEFAULT "" COMMENT 'HTTP请求行',
+                                  status STRING DEFAULT "" COMMENT 'HTTP状态码',
+                                  size STRING DEFAULT "" COMMENT '响应大小(byte)',
+                                  referer STRING DEFAULT "" COMMENT '来源页面',
+                                  `http/agent` STRING DEFAULT "" COMMENT 'User-Agent'
 )
-ENGINE=OLAP
-DUPLICATE KEY(wp_event_id)
-DISTRIBUTED BY HASH(wp_event_id) BUCKETS 8
+    ENGINE=OLAP
+    DUPLICATE KEY(`timestamp`)
+DISTRIBUTED BY HASH(`timestamp`) BUCKETS 8
 PROPERTIES (
     "replication_num" = "1"
 );
-select count(*) from test_db.wp_nginx
+
+
+DROP TABLE IF EXISTS test_db.wp_nginx_02;
+CREATE TABLE test_db.wp_nginx_02 (
+                                     `timestamp` bigint COMMENT '原始时间字符串',
+                                     wp_event_id STRING COMMENT '事件唯一ID',
+                                     wp_src_key STRING DEFAULT "" COMMENT '数据来源表示',
+                                     sip STRING DEFAULT "" COMMENT '客户端IP',
+                                     `http/request` STRING DEFAULT "" COMMENT 'HTTP请求行',
+                                     status STRING DEFAULT "" COMMENT 'HTTP状态码',
+                                     size STRING DEFAULT "" COMMENT '响应大小(byte)',
+                                     referer STRING DEFAULT "" COMMENT '来源页面',
+                                     `http/agent` STRING DEFAULT "" COMMENT 'User-Agent'
+)
+    ENGINE=OLAP
+    DUPLICATE KEY(`timestamp`)
+DISTRIBUTED BY HASH(`timestamp`) BUCKETS 8
+PROPERTIES (
+    "replication_num" = "1"
+);
+
+select * from test_db.wp_nginx_01;
+select * from test_db.wp_nginx_02;
